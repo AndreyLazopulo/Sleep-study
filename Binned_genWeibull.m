b91dadc46cd22412eaaa6e19e839f05f08f90a96
AndreyLazopulo <a.lazopulo@umiami.edu>
2017-11-21 15:41:37 -0500

    initial guess for alpha

--- Binned_genWeibull.m
+++ Binned_genWeibull.m
@@ -25,6 +25,9 @@ switch binway
 end
 
 [h,edges]=histcounts(distributdata,bin_bound);
+[f,gof]=fit(edges(1:5),-log(h(1:5)'./length(distributdata)),'poly1');
+CC=coeffvalues(f);
+a0=CC(1);
 h = reshape(h, 1, numel(h));
 edges = reshape(edges, 1, numel(edges));
 l=edges(1:end-1);
@@ -33,17 +36,17 @@ x=distributdata;
 x(x<xmin|x>xmax)=[];   %exclude data outside range
 
 meanx=mean(x); meanlnx=mean(log(x));  %%quantities that will be used later
-specoption=optimset('Algorithm','interior-point','MaxIter',10000,'MaxFunEvals',10000,'TolX',10^-6,'TolFun',10^-8,'Display','off','FinDiffType','central');
+specoption=optimset('Algorithm','interior-point','MaxIter',10000,'MaxFunEvals',10000,'TolX',10^-8,'TolFun',10^-8,'Display','off','FinDiffType','central');
 
 %%%initial values of alpha,a.k.a 'a' and beta, a.k.a 'b' come from the
 %%%approximate estimations of Gamma distribution on Wikipedia
-s=log(meanx)-meanlnx;
-a0=0.6;%(3-s+sqrt(24*s+(s-3)^2))/(12*s);
-b0=2.5;%meanx/a0;
-lam0=0.4;%1;
+% s=log(meanx)-meanlnx;
+% a0=(3-s+sqrt(24*s+(s-3)^2))/(12*s);
+b0=meanx/a0;
+lam0=1;
 % [param nLL]= fminsearch(@negloglike, [a0 b0 lam0]);
 % param= mle(x,'logpdf',@logpdf,'logsf',@logsf,'start',[a0 b0 lam0],'optimfun','fmincon','options', otheroptions2);
-param=fmincon(@logpdf,[a0 b0 lam0],[],[],[],[],[0 0 0],[1 Inf Inf],[],specoption);
+param=fmincon(@logpdf,[a0 b0 lam0],[],[],[],[],[a0-0.5*a0 0 0],[a0+0.5*a0 Inf Inf],[],specoption);
 alph=param(1); bet=param(2); lamd=param(3);
 % totLL=-numel(x)*nLL;
 totLL=logpdf(param);
